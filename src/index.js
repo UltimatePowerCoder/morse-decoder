46605a16bdb35a7162236d927cdf6ad3b8e897f7
Leonid Korshunov <ultimatepowercoder@gmail.com>
2024-10-22 13:14:12 +0400

    task complete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        const segment = expr.slice(i, i + 10);
+
+        if (segment === '**********') {
+            result += ' ';
+        } else {
+            let morseCode = '';
+            for (let j = 0; j < segment.length; j += 2) {
+                const pair = segment.slice(j, j + 2);
+                if (pair === '10') {
+                    morseCode += '.';
+                } else if (pair === '11') {
+                    morseCode += '-';
+                }
+            }
+            const letter = MORSE_TABLE[morseCode];
+            result += letter ? letter : '';
+        }
+    }
+    
+    return result;
 }
 
 module.exports = {
